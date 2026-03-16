a3ffb3ea7140c5eb4fdc063a44af465a7e4b7668
nabilban <nabilban354@gmail.com>
2026-03-16 11:14:12 +0700

    feat: add swagger and , docker compose for local development

--- cmd/main.go
+++ cmd/main.go
@@ -10,6 +10,16 @@ import (
 	"github.com/joho/godotenv"
 )
 
+// @title           Bizkit Backend API
+// @version         1.0
+// @description     API documentation for the Bizkit backend application.
+// @host            localhost:8080
+// @BasePath        /
+
+// @securityDefinitions.apikey BearerAuth
+// @in header
+// @name Authorization
+
 func main() {
 	godotenv.Load()
 
